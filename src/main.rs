6d0261b4473166e73e9fe4eddd20499ae44965ff
Vadzim Kuzmin <vadzimku@Vadzims-MacBook-Pro.local>
2024-03-28 22:05:07 +0200

    Add simple server sample

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+
+    // build our application with a route
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, Rust!"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    let user = User {
+        id: 42,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
